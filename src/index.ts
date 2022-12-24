1da771a6a0023f36f67978b4d368fc84710b715f
AndyLiu <85298826@qq.com>
2022-12-24 22:18:06 +0800

    增加工作流通用接口

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import chatCtrl from "./controller/chat"
 import userCtrl from "./controller/setting/userCtrl"
+import processCtrl from "./controller/setting/processCtrl"
 import appCtrl from "./controller/store/appCtrl"
 import docsCtrl from "./controller/store/docsCtrl"
 import marketCtrl from "./controller/store/marketCtrl"
-// import selfAppCtrl from "./controller/store/selfAppCtrl"
 import thingCtrl from "./controller/thing"
 import todoCtrl from "./controller/todo/todoCtrl"
+import {logger,LoggerLevel} from "./base/common/"
+
+import { IFileSystemItem, IObjectItem} from './core/';
+import { TargetType} from './core/enum';
+import { ICompany,SpaceType } from './core/target/itarget';
 
 /**
  * 顶层归口服务
- */
-export {docsCtrl,appCtrl,chatCtrl,marketCtrl,userCtrl,thingCtrl,todoCtrl}
+*/
+export {docsCtrl,appCtrl,chatCtrl,marketCtrl,userCtrl,thingCtrl,todoCtrl,processCtrl,logger}
+
+export type {IFileSystemItem,IObjectItem,TargetType,LoggerLevel,ICompany,SpaceType}
